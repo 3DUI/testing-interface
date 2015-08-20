e210684164b2625622c7b82dc911e0c19ad53500
steveryb <steveryb@gmail.com>
2015-08-20 19:19:18 +0200

    Rendered captured participant form in react

--- src/capture_participant_details.js
+++ src/capture_participant_details.js
@@ -1,19 +1,50 @@
-define(["jquery"], function($){
+define(["react"], function(React){
     return function(callback){
-        $("#experiment").hide();
-       var enteredNumber = function(){
-           return parseInt($("#participant_input").val());
-       };
-        
-       $("#submit-participant-number").click(function(){ 
-           var val = enteredNumber();
-           if(val){
-                window.log.meta.participantNumber = val;
-                $("#capture").hide();
-                callback();
-           } else {
-                alert("Participant numbers are only integers. Please enter a valid integer");
-           }
-       });
+        var CaptureParticipants = React.createClass({
+            handleChange: function(event){
+                this.setState({value: parseInt(event.target.value)});
+            },
+            submit: function(){
+               var val = this.state.value;
+               if(val){
+                    window.log.meta.participantNumber = val;
+                    React.unmountComponentAtNode(document.getElementById('content'));
+                    callback();
+               } else {
+                    alert("Participant numbers are only integers. Please enter a valid integer");
+               }
+            },
+            render: function(){
+                return <div id="capture" className="container-fluid">
+                    <div className="row">
+                        <div className="col-xs-6 col-xs-offset-3">
+                            <div className="row">
+                                <legend>Welcome</legend>
+                                Thank you for participating in our experiment! Before you begin, could you please read the informed consent form and instruction manual in front of you. After you've filled in the consent form, please enter your participant number below.
+                                <br />
+                                Once you've submitted, you will be taken to the experiment page.
+                            </div>
+                            <div className="row">
+                                <legend>Participant Information</legend>
+                                <div className="form-group">
+                                      <label className="col-md-4 control-label" for="participant_input">Particpant Number</label>  
+                                        <div className="col-md-5">
+                                              <input id="participant_input" name="participant_input" type="text" placeholder="12345" className="form-control input-md" min="0" step="1" data-bind="value:replyNumber" required="" onChange={this.handleChange} />
+                                                <span className="help-block">The participant number given to you, attached to your formal consent forms.</span>  
+                                       </div>
+                                </div>
+
+                              <button id="submit-participant-number" className="btn  btn-default" onClick={this.submit}>Submit</button>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            }
+        });
+
+        React.render(
+            <CaptureParticipants />,
+            document.getElementById("content")
+        );
     };
 });
