3834ab2bead7af65388524b16897dde3c0ca8841
steveryb <steveryb@gmail.com>
2015-08-31 11:06:57 +0200

    Updated documentation to reflect new interface

--- documentation/tasks.md
+++ documentation/tasks.md
@@ -8,13 +8,14 @@ Here we describe the interface as a whole, along with how to complete each task.
 
 The experimental interface has five sections. In the above, we have divided in into several vertical sections which we have explained below.
 
-1. __Title__: The title gives you the current controller you are testing, along with how many tasks you have left. Here, we are testing the Discrete Controller (shown in the _Model to Rotate_ screen in section 4) and we are on task 1 of 8.
+1. __Title__: The title gives you the current controller you are testing, along with how many tasks you have left. Here, we are in the training stage of using the Discrete Controller and we are on task 1 of 11.
 2. __Instructions__: This section gives you instructions for how to complete the current task. Here we can see we are completing an _Orientation Task_ where we have to rotate an object to match the orientation of another object. More full instructions for the types of tasks are given below.
-3. __Time taken__: This is a timer showing the time taken for the current task. 
+3. __Time Left__: This is a timer showing the time left for the current stage. This is only shown for training stages, as there is no time limit for evaluations. 
 4. __3D Views__: This section contains the 3D rendered scenes. This view depends on the type of task being completed, and the rotation controller being used.
-5. __Task Controls__: These buttons allow you to 
-    -  _Submit Task_: submit the current task when you have completed it.
-    - _Reset Orientation_: Reset the orientation of your model to its starting orientation.
+5. __Reference Orientation__: See the section on Orientation Tasks below.
+6. __Model to Rotate__: See the section on Orientation Tasks below.
+7. __Reset Model orientation__:Reset the orientation of your model to its starting orientation. 
+8. __Submit Task__:  Once you have completed the task, submit the task and start the next one.
 
 ## Orientation Matching
 
@@ -25,12 +26,8 @@ Orientation matching tasks involve you rotating a model to match the orientation
 - __Reference Orientation__: This is the orientation that you want to manipulate your model to match.
 - __Model to Rotate__: This is the model you will be rotating to match the _Reference Orientation's_ model.
 
-Please look at the controller documentation for how to manipulate the models.
-
 ## Inspection
 
 ![Inspection Tasks](images/inspection_task.png)
 
-Inspection matching tasks involve you trying to find a red patch on the model you're rotating. Here, there is only one screen: __Model to Rotate__. You must rotate this model until you find the red patch as shown in the image above.
-
-Please look at the controller documentation for how to manipulate the models.
+Inspection matching tasks involve you trying to find a red patch on the model you're rotating. Here, there is only one screen: __Model to Rotate__. You must rotate this model until you find the red patch as shown in the image above. Once you have, you should position the center of the controller on that red patch as shown above. The center of the controller is marked with a small black square. 
--- src/experiment_ui.js
+++ src/experiment_ui.js
@@ -3,30 +3,30 @@ define(["react"], function(React){
         render: function(){
             var timerDiv = this.props.timed === "true" ? <h4 id="timer"></h4> : "";
             var controllerButtons = this.props.showControllerSelection === "true" ? 
-                        <div classname="row" style={{display:"none"}}>
+                        <div className="row" style={{display:"none"}}>
                             <span><h5>Select rotation controller</h5></span>
                             <button id="discrete" className="btn btn-default">Discrete</button>
                             <button id="two-axis" className="btn btn-default">Two-Axis Valuator</button>
                             <button id="arcball" className="btn btn-default">Arcball</button>
                         </div> : "";
-            return <div id="experiment" className="container-fluid" style={{padding:0+'px'}}>
-                <div classname="row">
+            return <div id="experiment" className="container-fluid">
+                <div className="row">
                     <legend id="header-tasks"></legend>
                     <h4 id="header-explanation"></h4>
                     {timerDiv}
                 </div>
-                <div classname="row" style={{position:'relative'}} id="three"></div>
-                <div classname="row" id="orientation-labels">
+                <div className="row" style={{position:'relative'}} id="three"></div>
+                <div className="row" id="orientation-labels">
                     <div className="col-xs-6">
                         <center>
-                        <div classname="row">
+                        <div className="row">
                             <h4>Reference Orientation</h4>
                         </div>
                         </center>
                     </div>
                     <div className="col-xs-6">
                         <center>
-                        <div classname="row">
+                        <div className="row">
                             <h4>Model to Rotate</h4>
                         </div>
                         </center>
@@ -35,25 +35,24 @@ define(["react"], function(React){
                 <div className="row" id="inspection-labels">
                     <div className="col-xs-12">
                         <center>
-                        <div classname="row">
+                        <div className="row">
                             <h4>Model to Rotate</h4>
                         </div>
                         </center>
                     </div>
                 </div>
-                <div classname="row">
+                <div className="row">
                     <div className="col-xs-6 controls">
                         <center>
-                        <div classname="row">
+                        <div className="row">
                             <button id="reload" style={{margin:"5px"}} className="btn btn-default">Reset Model Orientation</button>
-                            <button id="undo" className="btn btn-default">Undo Task Submission</button>
                         </div>
                         {controllerButtons}
                         </center>
                     </div>
                     <div className="col-xs-6">
                         <center>
-                        <div classname="row">
+                        <div className="row">
                             <button id="save" className="btn btn-default">Submit Task</button>
                         </div>
                         </center>
